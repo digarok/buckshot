323a7142fc460b3aad20ef496589a08d4bfe0142
Dagen Brock <dagenbrock@gmail.com>
2017-01-15 20:17:29 -0600

    fix for paths with spaces (linux) - reported by Michal Lutinsky

--- platform/linux/buckshot.sh
+++ platform/linux/buckshot.sh
@@ -5,8 +5,8 @@ dirname=`dirname $0`
 tmp="${dirname#?}"
 
 if [ "${dirname%$tmp}" != "/" ]; then
-dirname=$PWD/$dirname
+dirname="$PWD/$dirname"
 fi
-LD_LIBRARY_PATH=$dirname/lib
+LD_LIBRARY_PATH="$dirname/lib"
 export LD_LIBRARY_PATH
-$dirname/$appname "$@"
+"$dirname/$appname" "$@"
